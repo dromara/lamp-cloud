7faf1d8c865996568d47182c7bbe5491a64a24aa
zuihou <244387066@qq.com>
2018-01-13 21:13:43 +0800

    1，eureka 高可用配置 2，config 注册到eureka

---
+++ zuihou-backend/zuihou-admin/src/main/java/com/github/zuihou/admin/AdminApplication.java
@@ -0,0 +1,17 @@
+package com.github.zuihou.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author tyh
+ * @createTime 2018-01-13 1:34
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
---
+++ zuihou-backend/zuihou-admin/src/main/java/com/github/zuihou/admin/config/Swagger2.java
@@ -0,0 +1,80 @@
+//package com.github.zuihou.admin.config;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+//import springfox.documentation.builders.ApiInfoBuilder;
+//import springfox.documentation.builders.PathSelectors;
+//import springfox.documentation.builders.RequestHandlerSelectors;
+//import springfox.documentation.service.ApiInfo;
+//import springfox.documentation.service.Contact;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spring.web.plugins.Docket;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//
+///**
+// * Swagger2的配置类
+// *
+// * @author xubin.
+// * @create 2017-04-08
+// */
+//
+//@Configuration
+//@EnableSwagger2
+//public class Swagger2 extends WebMvcConfigurerAdapter {
+//
+//    @Value("${swagger2.package}")
+//    private String basePackage;
+//
+//    @Value("${swagger2.service.name:spring.application.name}")
+//    private String title;
+//    @Value("${swagger2.service.description:null}")
+//    private String description;
+//    @Value("${swagger2.service.contact.name:null}")
+//    private String contactName;
+//    @Value("${swagger2.service.contact.url:null}")
+//    private String contactUrl;
+//    @Value("${swagger2.service.contact.email:null}")
+//    private String contactEmail;
+//
+//    /**
+//     * 这个地方要重新注入一下资源文件，不然不会注入资源的，也没有注入requestHandlerMappping,相当于xml配置的
+//     * <!--swagger资源配置-->
+//     * <mvc:resources location="classpath:/META-INF/resources/" mapping="swagger-ui.html"/>
+//     * <mvc:resources location="classpath:/META-INF/resources/webjars/" mapping="/webjars/**"/>
+//     * 不知道为什么，这也是spring boot的一个缺点（菜鸟觉得的）
+//     *
+//     * @param registry
+//     */
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("swagger-ui.html")
+//                .addResourceLocations("classpath:/META-INF/resources/");
+//        registry.addResourceHandler("/webjars*")
+//                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+//    }
+//
+//    @Bean
+//    public Docket createRestApi() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .apiInfo(apiInfo())
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage(basePackage))
+//                .paths(PathSelectors.any())
+//                .build();
+//    }
+//
+//    private ApiInfo apiInfo() {
+//        Contact contact = new Contact(contactName, contactUrl, contactEmail);
+//        return new ApiInfoBuilder()
+//                .title(title + " RESTful APIs")
+//                .description(description)
+//                .contact(contact)
+//                .version("1.0")
+//                .build();
+//    }
+//
+//
+//}
