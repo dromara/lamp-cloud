b858fc11b58e5b99147931d54aa7e4b47aa551c9
zuihou <244387066@qq.com>
2018-01-14 00:30:23 +0800

    调整logback日志

--- zuihou-backend/zuihou-admin/src/main/java/com/github/zuihou/admin/controller/TContr.java
+++ zuihou-backend/zuihou-admin/src/main/java/com/github/zuihou/admin/controller/TContr.java
@@ -1,5 +1,8 @@
 package com.github.zuihou.admin.controller;
 
+import com.github.zuihou.admin.ser.Ser;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,14 +16,23 @@ import org.springframework.web.bind.annotation.RestController;
 //加了这个注解，刷新才能生效
 @RefreshScope
 @RestController
+@Slf4j
 public class TContr {
     @Value("${test:null}")
     private String test;
     @Value("${hello:null}")
     private String hello;
+    @Autowired
+    Ser ser;
 
     @RequestMapping("t")
     public String test(){
+        log.error("info---------------");
+        log.warn("warn---------------");
+        log.info("info---------------");
+        log.debug("debug---------------");
+        log.trace("trace---------------");
+        ser.test();
         return test + "==" + hello;
     }
 
