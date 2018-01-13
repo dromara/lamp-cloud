e1dadcd7b8f6d188445751193b9af9b3c2e3b700
zuihou <244387066@qq.com>
2018-01-13 23:32:41 +0800

    调试admin项目 高可用配置

--- zuihou-backend/zuihou-admin/src/main/java/com/github/zuihou/admin/controller/TContr.java
+++ zuihou-backend/zuihou-admin/src/main/java/com/github/zuihou/admin/controller/TContr.java
@@ -1,23 +1,27 @@
 package com.github.zuihou.admin.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ *
  * @author tyh
  * @createTime 2018-01-13 1:42
  */
+//加了这个注解，刷新才能生效
+@RefreshScope
 @RestController
 public class TContr {
     @Value("${test:null}")
     private String test;
-    @Value("${test2:null}")
-    private String test2;
+    @Value("${hello:null}")
+    private String hello;
 
     @RequestMapping("t")
     public String test(){
-        return test + "==" + test2;
+        return test + "==" + hello;
     }
 
 }
