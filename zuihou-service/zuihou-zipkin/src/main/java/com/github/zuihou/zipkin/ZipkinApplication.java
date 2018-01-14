67dc8f87139d69b058950f4d6c64551848d0e7a3
zuihou <244387066@qq.com>
2018-01-14 17:23:32 +0800

    增加zipkin，spring-admin-boot监控

--- zuihou-service/zuihou-zipkin/src/main/java/com/github/zuihou/zipkin/ZipkinApplication.java
+++ zuihou-service/zuihou-zipkin/src/main/java/com/github/zuihou/zipkin/ZipkinApplication.java
@@ -6,14 +6,13 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 
 /**
+ * 启动类
  * @author tyh
  * @createTime 2018-01-14 12:04
  */
 @SpringBootApplication
-//注册到eureka
-@EnableDiscoveryClient
-//使用Stream方式启动ZipkinServer
-@EnableZipkinStreamServer
+@EnableDiscoveryClient //注册到eureka
+@EnableZipkinStreamServer //使用Stream方式启动ZipkinServer
 public class ZipkinApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZipkinApplication.class, args);
