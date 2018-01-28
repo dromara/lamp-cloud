affc4f2e141f7ab1472bdc38866cb6123439e1a3
zuihou <244387066@qq.com>
2018-01-28 16:36:22 +0800

    调整

---
+++ zuihou-backend/zuihou-file/zuihou-file-api/src/main/java/com/github/zuihou/file/rest/file/constant/FileType.java
@@ -0,0 +1,12 @@
+package com.github.zuihou.file.rest.file.constant;
+
+/**
+ * @author tyh
+ * @createTime 2018-01-28 2:04
+ */
+public enum FileType {
+    /** api */
+    API,
+    /** 文件系统 */
+    SYSTEM,;
+}
