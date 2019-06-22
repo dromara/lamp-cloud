e451de3fdccc84029d92fd9092003227eaf3d0df
tangyh <244387066@qq.com>
2019-06-22 22:10:25 +0800

    重新设计表，设计系统的模块划分

---
+++ zuihou-backend/zuihou-authority/zuihou-authority-server/src/main/java/com/github/zuihou/admin/AuthorityApplication.java
@@ -0,0 +1,36 @@
+package com.github.zuihou.admin;
+
+import java.net.InetAddress;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author zuihou
+ * @createTime 2018-01-13 1:34
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableTransactionManagement
+//@EnableAuthServer
+@Configuration
+@Slf4j
+public class AuthorityApplication {
+    public static void main(String[] args) throws Exception {
+        ConfigurableApplicationContext application = SpringApplication.run(AuthorityApplication.class, args);
+        Environment env = application.getEnvironment();
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "应用 '{}' 运行成功! 访问连接:\n\t" +
+                        "Swagger文档: \t\thttp://{}:{}/doc.html\n" +
+                        "----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"));
+    }
+}
