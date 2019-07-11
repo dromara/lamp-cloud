2c322d898e0a4e6dd2fa2d686af4d41706515b6f
tangyh <244387066@qq.com>
2019-07-11 17:55:31 +0800

    配置跟pom

--- zuihou-frontend/zuihou-manage-center/src/main/java/com/github/zuihou/center/config/CenterConfiguration.java
+++ zuihou-frontend/zuihou-manage-center/src/main/java/com/github/zuihou/center/config/CenterConfiguration.java
@@ -1,8 +1,5 @@
 package com.github.zuihou.center.config;
 
-import java.util.ArrayList;
-import java.util.Collections;
-
 import com.github.zuihou.common.adapter.BaseConfig;
 
 import org.springframework.context.annotation.Configuration;
@@ -12,22 +9,22 @@ import org.springframework.context.annotation.Primary;
 @Primary
 public class CenterConfiguration extends BaseConfig {
 
-    @Override
-    protected ArrayList<String> getExcludeCommonPathPatterns() {
-        ArrayList<String> list = new ArrayList<>();
-        String[] urls = {
-                "/v2/api-docs",
-                "/swagger-resources/**",
-                "/client/**",
-                "/login",
-                "/register",
-                "/jwt/**"
-        };
-        Collections.addAll(list, urls);
-        ArrayList<String> superList = super.getExcludeCommonPathPatterns();
-        Collections.addAll(list, superList.toArray(new String[superList.size()]));
-        return list;
-    }
+//    @Override
+//    protected ArrayList<String> getExcludeCommonPathPatterns() {
+//        ArrayList<String> list = new ArrayList<>();
+//        String[] urls = {
+//                "/v2/api-docs",
+//                "/swagger-resources/**",
+//                "/client/**",
+//                "/login",
+//                "/register",
+//                "/jwt/**"
+//        };
+//        Collections.addAll(list, urls);
+//        ArrayList<String> superList = super.getExcludeCommonPathPatterns();
+//        Collections.addAll(list, superList.toArray(new String[superList.size()]));
+//        return list;
+//    }
 
 
 }
