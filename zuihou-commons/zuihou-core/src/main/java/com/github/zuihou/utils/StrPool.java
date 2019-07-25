bba2dca215716ed5576670c435f383c2f32f4d46
tangyh <244387066@qq.com>
2019-07-25 21:18:21 +0800

    统一常量字符串工具类

--- zuihou-commons/zuihou-core/src/main/java/com/github/zuihou/utils/StrPool.java
+++ zuihou-commons/zuihou-core/src/main/java/com/github/zuihou/utils/StrPool.java
@@ -11,19 +11,30 @@ public interface StrPool {
     String AND = "and";
     String AT = "@";
     String ASTERISK = "*";
-    String STAR = ASTERISK;
+    String STAR = "*";
+    String BACK_SLASH = "\\";
+    String COLON = ":";
+    String COMMA = ",";
+    String DASH = "-";
     String DOLLAR = "$";
+    String DOT = ".";
+    String DOTDOT = "..";
     String DOT_CLASS = ".class";
     String DOT_JAVA = ".java";
     String DOT_XML = ".xml";
+    String EMPTY = "";
     String EQUALS = "=";
     String FALSE = "false";
+    String SLASH = "/";
     String HASH = "#";
     String HAT = "^";
+    String LEFT_BRACE = "{";
     String LEFT_BRACKET = "(";
     String LEFT_CHEV = "<";
+    String NEWLINE = "\n";
     String N = "n";
     String NO = "no";
+    String NULL = "null";
     String OFF = "off";
     String ON = "on";
     String PERCENT = "%";
@@ -32,7 +43,9 @@ public interface StrPool {
     String QUESTION_MARK = "?";
     String EXCLAMATION_MARK = "!";
     String QUOTE = "\"";
+    String RETURN = "\r";
     String TAB = "\t";
+    String RIGHT_BRACE = "}";
     String RIGHT_BRACKET = ")";
     String RIGHT_CHEV = ">";
     String SEMICOLON = ";";
@@ -40,7 +53,10 @@ public interface StrPool {
     String BACKTICK = "`";
     String SPACE = " ";
     String TILDA = "~";
+    String LEFT_SQ_BRACKET = "[";
+    String RIGHT_SQ_BRACKET = "]";
     String TRUE = "true";
+    String UNDERSCORE = "_";
     String UTF_8 = "UTF-8";
     String US_ASCII = "US-ASCII";
     String ISO_8859_1 = "ISO-8859-1";
@@ -50,9 +66,14 @@ public interface StrPool {
     String ZERO = "0";
     String DOLLAR_LEFT_BRACE = "${";
     String HASH_LEFT_BRACE = "#{";
+    String CRLF = "\r\n";
+    String HTML_NBSP = "&nbsp;";
+    String HTML_AMP = "&amp";
+    String HTML_QUOTE = "&quot;";
+    String HTML_LT = "&lt;";
+    String HTML_GT = "&gt;";
 
 
-    String NULL = "null";
     String TEST = "test";
     String PROD = "prod";
 }
