f6c7f8d443df6ec44bb0a6669c5229f4806b7e2e
tangyh <244387066@qq.com>
2019-07-25 17:34:47 +0800

    通用 controller

--- zuihou-backend/zuihou-authority/zuihou-authority-server/src/main/java/com/github/zuihou/general/controller/CommonController.java
+++ zuihou-backend/zuihou-authority/zuihou-authority-server/src/main/java/com/github/zuihou/general/controller/CommonController.java
@@ -1,4 +1,4 @@
-package com.github.zuihou.common.controller;
+package com.github.zuihou.general.controller;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,6 +14,7 @@ import com.github.zuihou.common.enums.HttpMethod;
 import com.github.zuihou.database.mybatis.auth.DataScopeType;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,12 +28,13 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @Slf4j
 @RestController
-@Api(value = "Common", tags = "公共Controller")
+@Api(value = "Common", tags = "通用Controller")
 public class CommonController {
 
     @Autowired
     private DictionaryService dictionaryService;
 
+    @ApiOperation(value = "获取当前系统所有枚举", notes = "获取当前系统所有枚举")
     @GetMapping("/enums")
     public R<Map<String, Object>> enums() {
         Map<String, Object> map = new HashMap<>(7);
@@ -46,10 +48,10 @@ public class CommonController {
         return R.success(map);
     }
 
+    @ApiOperation(value = "获取当前系统所有数据字典", notes = "获取当前系统所有数据字典")
     @GetMapping("/dictionary")
     public R<Map<String, Object>> dictionary() {
         Map<String, Object> map = new HashMap<>();
-
         return R.success(map);
     }
 
