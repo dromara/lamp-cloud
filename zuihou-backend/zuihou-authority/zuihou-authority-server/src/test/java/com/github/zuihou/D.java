f48dd1bd9e4854568ca66446c0f552e925881c73
tangyh <244387066@qq.com>
2019-07-25 18:08:57 +0800

    添加 通过 feign Client 上传附件的接口

---
+++ zuihou-backend/zuihou-authority/zuihou-authority-server/src/test/java/com/github/zuihou/D.java
@@ -0,0 +1,20 @@
+package com.github.zuihou;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+/**
+ * This is a Description
+ *
+ * @author tangyh
+ * @date 2019/07/25
+ */
+public class D {
+    private LocalDateTime date;
+    private Date d2;
+}
---
+++ zuihou-backend/zuihou-authority/zuihou-authority-server/src/test/java/com/github/zuihou/D2.java
@@ -0,0 +1,18 @@
+package com.github.zuihou;
+
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * This is a Description
+ *
+ * @author tangyh
+ * @date 2019/07/25
+ */
+
+@Data
+@ToString
+public class D2 {
+    private String date;
+    private String d2;
+}
