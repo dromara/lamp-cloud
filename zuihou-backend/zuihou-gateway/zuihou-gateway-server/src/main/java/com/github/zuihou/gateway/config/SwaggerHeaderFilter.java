c733f2112523da7a04baf748a70526efc8c54322
tangyh <244387066@qq.com>
2019-07-31 14:52:01 +0800

    尝试增加 gateway 组件， 但还未完全调试成功， 目前项目还是需要使用 zuihou-zuul-server

---
+++ zuihou-backend/zuihou-gateway/zuihou-gateway-server/src/main/java/com/github/zuihou/gateway/config/SwaggerHeaderFilter.java
@@ -0,0 +1,37 @@
+//package com.github.zuihou.gateway.config;
+//
+//import org.apache.commons.lang3.StringUtils;
+//import org.springframework.cloud.gateway.filter.GatewayFilter;
+//import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+//import org.springframework.http.server.reactive.ServerHttpRequest;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.server.ServerWebExchange;
+//
+///**
+// * This is a Description
+// *
+// * @author tangyh
+// * @date 2019/07/31
+// */
+//
+//@Component
+//public class SwaggerHeaderFilter extends AbstractGatewayFilterFactory {
+//    private static final String HEADER_NAME = "X-Forwarded-Prefix";
+//
+//    private static final String URI = "/v2/api-docs";
+//
+//    @Override
+//    public GatewayFilter apply(Object config) {
+//        return (exchange, chain) -> {
+//            ServerHttpRequest request = exchange.getRequest();
+//            String path = request.getURI().getPath();
+//            if (!StringUtils.endsWithIgnoreCase(path, URI)) {
+//                return chain.filter(exchange);
+//            }
+//            String basePath = path.substring(0, path.lastIndexOf(URI));
+//            ServerHttpRequest newRequest = request.mutate().header(HEADER_NAME, basePath).build();
+//            ServerWebExchange newExchange = exchange.mutate().request(newRequest).build();
+//            return chain.filter(newExchange);
+//        };
+//    }
+//}
---
+++ zuihou-backend/zuihou-gateway/zuihou-gateway-server/src/main/java/com/github/zuihou/gateway/controller/GateController.java
@@ -0,0 +1,21 @@
+package com.github.zuihou.gateway.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * This  Description
+ *
+ * @author tangyh
+ * @date 2019/07/31
+ */
+@RestController
+public class GateController {
+
+    @GetMapping("/test")
+    @ResponseBody
+    public String dictionaryAndEnum(String name) {
+        return "123";
+    }
+}
