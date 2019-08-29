ed7a0d0dc409b195d2a9bc7d3f7154c4efa7c80b
tangyh <244387066@qq.com>
2019-08-29 13:45:43 +0800

    1,修复bug 2，增加测试类

---
+++ zuihou-backend/zuihou-demo/zuihou-demo-biz/src/main/java/com/github/zuihou/demo/service/CCommonAreaService.java
@@ -0,0 +1,17 @@
+package com.github.zuihou.demo.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.github.zuihou.demo.entity.CCommonArea;
+
+/**
+ * <p>
+ * 业务接口
+ * 地区表
+ * </p>
+ *
+ * @author zuihou
+ * @date 2019-08-20
+ */
+public interface CCommonAreaService extends IService<CCommonArea> {
+
+}
