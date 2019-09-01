707fafd26b00b4409b2b681018870ee7e711e8f8
tangyh <244387066@qq.com>
2019-09-01 22:18:17 +0800

    完善消息中心相关接口，并完成前端对接

--- zuihou-backend/zuihou-msgs/zuihou-msgs-entity/src/main/java/com/github/zuihou/msgs/dto/MsgsCenterInfoQueryDTO.java
+++ zuihou-backend/zuihou-msgs/zuihou-msgs-entity/src/main/java/com/github/zuihou/msgs/dto/MsgsCenterInfoQueryDTO.java
@@ -1,6 +1,7 @@
 package com.github.zuihou.msgs.dto;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 import com.github.zuihou.msgs.enumeration.MsgsCenterType;
 
@@ -26,4 +27,14 @@ public class MsgsCenterInfoQueryDTO implements Serializable {
     private Boolean isRead;
     @ApiModelProperty(value = "消息类型")
     private MsgsCenterType msgsCenterType;
+
+    @ApiModelProperty(value = "内容")
+    private String content;
+    @ApiModelProperty(value = "标题")
+    private String title;
+    @ApiModelProperty(value = "开始时间")
+    private LocalDateTime startCreateTime;
+    @ApiModelProperty(value = "结束时间")
+    private LocalDateTime endCreateTime;
+
 }
