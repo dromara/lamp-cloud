946975bc9a79ee9d42771dd35e78066186d123fc
tangyh <244387066@qq.com>
2019-09-07 09:11:40 +0800

    1,完善文档 2，完善部分项目的日志路径错误的情况

--- docs/dockerfile/nginx/start.sh
+++ docs/dockerfile/nginx/start.sh
@@ -3,7 +3,7 @@
 docker stop zuihou_nginx
 docker rm zuihou_nginx
 docker run -idt -p 10000:10000 --name zuihou_nginx --restart=always \
-    -v /data/docker-data/projects/:/projects \
+    -v /data/projects/:/data/projects \
     -v `pwd`/conf/:/etc/nginx \
     -v `pwd`/logs/:/var/log/nginx  \
     -e TZ="Asia/Shanghai" \
