3a2ae69c8f3c85c15a252e2fd6285cb8b8c3b1c0
tangyh <244387066@qq.com>
2019-09-18 00:03:21 +0800

    1，将nacos中的配置和项目resources下的配置文件整理清洁一些，使得修改配置更加方便 2，增加UndertowServerFactoryCustomizer，解决启动是的警告 3，完善docker文档 4，同步nacos最新的配置 5，整理seata的配置

---
+++ docs/dockerfile/nacos/start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+docker stop zuihou-nacos
+docker rm zuihou-nacos
+docker run -idt --name zuihou-nacos --restart=always \
+        -e JVM_XMS=512m -e JVM_XMX=512m -e JVM_XMN=384m \
+        -e PREFER_HOST_MODE=hostname -e MODE=standalone -e SPRING_DATASOURCE_PLATFORM=mysql \
+        -e MYSQL_DATABASE_NUM=1 \
+        -e MYSQL_MASTER_SERVICE_HOST=192.168.1.34 -e MYSQL_MASTER_SERVICE_DB_NAME=nacos_test -e MYSQL_MASTER_SERVICE_PORT=3306 \
+        -e MYSQL_MASTER_SERVICE_USER=root \
+        -e MYSQL_MASTER_SERVICE_PASSWORD="root" \
+        -p 8848:8848 \
+        -v `pwd`/logs/:/home/nacos/logs \
+        -v `pwd`/init.d/custom.properties:/home/nacos/init.d/custom.properties \
+        nacos/nacos-server:1.1.3
