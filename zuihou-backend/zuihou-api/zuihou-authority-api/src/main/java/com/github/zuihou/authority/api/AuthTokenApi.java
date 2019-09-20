01ec84691b1b72a6256e225386e5f5c9aa238a97
tangyh <244387066@qq.com>
2019-09-20 23:00:16 +0800

    调整zuihou-config模块的路径

--- zuihou-backend/zuihou-api/zuihou-authority-api/src/main/java/com/github/zuihou/authority/api/AuthTokenApi.java
+++ zuihou-backend/zuihou-api/zuihou-authority-api/src/main/java/com/github/zuihou/authority/api/AuthTokenApi.java
@@ -1,41 +1,41 @@
-package com.github.zuihou.authority.api;
-
-import com.github.zuihou.authority.api.hystrix.AuthTokenApiHystrix;
-import com.github.zuihou.authority.dto.auth.LoginDTO;
-import com.github.zuihou.base.R;
-
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-
-/**
- * 认证Token API 接口
- *
- * @author zuihou
- * @date 2019/06/25
- */
-@FeignClient(name = "${zuihou.feign.authority-server:zuihou-authority-server}", fallback = AuthTokenApiHystrix.class)
-public interface AuthTokenApi {
-
-    /**
-     * 刷新token
-     *
-     * @param account  登录账号
-     * @param password
-     * @return
-     */
-//    @RequestMapping(value = "/anno/token", method = RequestMethod.GET)
-//    R<Token> token(@RequestParam("account") String account, @RequestParam("password") String password);
-
-    /**
-     * 登录接口
-     *
-     * @param account  登录账号
-     * @param password
-     * @return
-     */
-    @RequestMapping(value = "/anno/login", method = RequestMethod.POST)
-    R<LoginDTO> login(@RequestParam("account") String account, @RequestParam("password") String password);
-
-}
+//package com.github.zuihou.authority.api;
+//
+//import com.github.zuihou.authority.api.hystrix.AuthTokenApiHystrix;
+//import com.github.zuihou.authority.dto.auth.LoginDTO;
+//import com.github.zuihou.base.R;
+//
+//import org.springframework.cloud.openfeign.FeignClient;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RequestParam;
+//
+///**
+// * 认证Token API 接口
+// *
+// * @author zuihou
+// * @date 2019/06/25
+// */
+//@FeignClient(name = "${zuihou.feign.authority-server:zuihou-authority-server}", fallback = AuthTokenApiHystrix.class)
+//public interface AuthTokenApi {
+//
+//    /**
+//     * 刷新token
+//     *
+//     * @param account  登录账号
+//     * @param password
+//     * @return
+//     */
+////    @RequestMapping(value = "/anno/token", method = RequestMethod.GET)
+////    R<Token> token(@RequestParam("account") String account, @RequestParam("password") String password);
+//
+//    /**
+//     * 登录接口
+//     *
+//     * @param account  登录账号
+//     * @param password
+//     * @return
+//     */
+//    @RequestMapping(value = "/anno/login", method = RequestMethod.POST)
+//    R<LoginDTO> login(@RequestParam("account") String account, @RequestParam("password") String password);
+//
+//}
