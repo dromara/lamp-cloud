132e3680ea0556d313e1c0b19e4da4e48b8d4d26
tangyh <244387066@qq.com>
2019-10-21 00:09:16 +0800

    # 2019年10月20日23:46:29 1, 新增全新UI： https://github.com/zuihou/zuihou-ui 2, 对接新UI的验证码 + 登录 + 动态获取路由 3，新增登录日志表 4，修改权限库多张表的结构（zuihou_authority_new.sql) 5，修改了nacos中的配置文件 common.yml 、zuihou-authority-server-dev.yml 6，完善代码生成器 7，修复 zuihou-jobs-server 启动失败的bug 8， 项目文档程序员节5折(10月24日)： https://www.kancloud.cn/zuihou/zuihou-admin-cloud

---
+++ zuihou-backend/zuihou-authority/zuihou-authority-biz/src/main/java/com/github/zuihou/authority/dao/common/LoginLogMapper.java
@@ -0,0 +1,20 @@
+package com.github.zuihou.authority.dao.common;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.github.zuihou.authority.entity.common.LoginLog;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * <p>
+ * Mapper 接口
+ * 系统日志
+ * </p>
+ *
+ * @author zuihou
+ * @date 2019-10-20
+ */
+@Repository
+public interface LoginLogMapper extends BaseMapper<LoginLog> {
+
+}
