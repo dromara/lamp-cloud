488415835065741ef4df019904be520494980198
tangyh <244387066@qq.com>
2019-10-21 00:09:16 +0800

    # 2019年10月20日23:46:29 1, 新增全新UI： https://github.com/zuihou/zuihou-ui 2, 对接新UI的验证码 + 登录 + 动态获取路由 3，新增登录日志表 4，修改权限库多张表的结构（zuihou_authority_new.sql) 5，修改了nacos中的配置文件 common.yml 、zuihou-authority-server-dev.yml 6，完善代码生成器 7，修复 zuihou-jobs-server 启动失败的bug 8， 项目文档程序员节5折(10月24日)： https://www.kancloud.cn/zuihou/zuihou-admin-cloud

---
+++ zuihou-backend/zuihou-authority/zuihou-authority-entity/src/main/java/com/github/zuihou/authority/entity/common/LoginLog.java
@@ -0,0 +1,104 @@
+package com.github.zuihou.authority.entity.common;
+
+import java.time.LocalDateTime;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.github.zuihou.base.entity.SuperEntity;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+import org.hibernate.validator.constraints.Length;
+
+import static com.baomidou.mybatisplus.annotation.SqlCondition.LIKE;
+
+/**
+ * <p>
+ * 实体类
+ * 系统日志
+ * </p>
+ *
+ * @author zuihou
+ * @since 2019-10-20
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+@Accessors(chain = true)
+@TableName("c_common_login_log")
+@ApiModel(value = "LoginLog", description = "系统日志")
+public class LoginLog extends SuperEntity<Long> {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 操作IP
+     */
+    @ApiModelProperty(value = "操作IP")
+    @Length(max = 50, message = "操作IP长度不能超过50")
+    @TableField(value = "request_ip", condition = LIKE)
+    private String requestIp;
+
+    /**
+     * 登录用户
+     */
+    @ApiModelProperty(value = "登录用户")
+    @Length(max = 50, message = "登录用户长度不能超过50")
+    @TableField(value = "user_name", condition = LIKE)
+    private String userName;
+
+    /**
+     * 登录描述
+     */
+    @ApiModelProperty(value = "登录描述")
+    @Length(max = 255, message = "登录描述长度不能超过255")
+    @TableField(value = "description", condition = LIKE)
+    private String description;
+
+    /**
+     * 开始时间
+     */
+    @ApiModelProperty(value = "开始时间")
+    @TableField("login_time")
+    private LocalDateTime loginTime;
+
+    /**
+     * 浏览器请求头
+     */
+    @ApiModelProperty(value = "浏览器请求头")
+    @TableField("ua")
+    private Long ua;
+
+    /**
+     * 登录地点
+     */
+    @ApiModelProperty(value = "登录地点")
+    @Length(max = 50, message = "登录地点长度不能超过50")
+    @TableField(value = "location", condition = LIKE)
+    private String location;
+
+
+    @Builder
+    public LoginLog(Long id, LocalDateTime createTime, Long createUser,
+                    String requestIp, String userName, String description, LocalDateTime loginTime, Long ua, String location) {
+        this.id = id;
+        this.createTime = createTime;
+        this.createUser = createUser;
+        this.requestIp = requestIp;
+        this.userName = userName;
+        this.description = description;
+        this.loginTime = loginTime;
+        this.ua = ua;
+        this.location = location;
+    }
+
+}
