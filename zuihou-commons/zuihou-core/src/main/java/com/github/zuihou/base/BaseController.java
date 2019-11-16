5c607b171c14929156f0ae71038c52d189fb00cd
tangyh <244387066@qq.com>
2019-11-16 16:09:48 +0800

    对接录日志& 操作日志

--- zuihou-commons/zuihou-core/src/main/java/com/github/zuihou/base/BaseController.java
+++ zuihou-commons/zuihou-core/src/main/java/com/github/zuihou/base/BaseController.java
@@ -1,5 +1,8 @@
 package com.github.zuihou.base;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,6 +13,10 @@ import com.github.zuihou.exception.BizException;
 import com.github.zuihou.exception.code.BaseExceptionCode;
 import com.github.zuihou.utils.AntiSqlFilter;
 import com.github.zuihou.utils.NumberHelper;
+
+import cn.hutool.core.util.StrUtil;
+
+import static com.github.zuihou.utils.DateUtils.DEFAULT_DATE_TIME_FORMAT;
 
 /**
  * SuperController
@@ -38,6 +45,9 @@ public abstract class BaseController {
      * 排序字段 DESC
      */
     protected static final String PAGE_DESCS = "descs";
+
+    protected static final String START_CREATE_TIME = "startCreateTime";
+    protected static final String END_CREATE_TIME = "endCreateTime";
     /**
      * 默认每页条目20,最大条目数100
      */
@@ -117,6 +127,7 @@ public abstract class BaseController {
     protected String getTenant() {
         return BaseContextHandler.getTenant();
     }
+
     protected String getAccount() {
         return BaseContextHandler.getAccount();
     }
@@ -176,5 +187,25 @@ public abstract class BaseController {
      */
     protected String[] getParameterSafeValues(String parameter) {
         return AntiSqlFilter.getSafeValues(request.getParameterValues(parameter));
+    }
+
+    protected LocalDateTime getStartCreateTime() {
+        return getLocalDateTime(START_CREATE_TIME);
+    }
+
+    protected LocalDateTime getEndCreateTime() {
+        return getLocalDateTime(END_CREATE_TIME);
+    }
+
+    private LocalDateTime getLocalDateTime(String endCreateTime) {
+        String startCreateTime = request.getParameter(endCreateTime);
+        if (StrUtil.isBlank(startCreateTime)) {
+            return null;
+        }
+        String safeValue = AntiSqlFilter.getSafeValue(startCreateTime);
+        if (StrUtil.isBlank(safeValue)) {
+            return null;
+        }
+        return LocalDateTime.parse(safeValue, DateTimeFormatter.ofPattern(DEFAULT_DATE_TIME_FORMAT));
     }
 }
