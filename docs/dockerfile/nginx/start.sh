6a81878421dbdce8537511bc524a5f3a1f28f457
tangyh <244387066@qq.com>
2019-12-02 13:58:30 +0800

    完善日志

--- docs/dockerfile/nginx/start.sh
+++ docs/dockerfile/nginx/start.sh
@@ -2,7 +2,7 @@
 
 docker stop zuihou_nginx
 docker rm zuihou_nginx
-docker run -idt -p 10000:10000 --name zuihou_nginx --restart=always \
+docker run -idt -p 10000:10000 -p 180:180 --name zuihou_nginx --restart=always \
     -v /data/projects/:/data/projects \
     -v `pwd`/conf/:/etc/nginx \
     -v `pwd`/logs/:/var/log/nginx  \
