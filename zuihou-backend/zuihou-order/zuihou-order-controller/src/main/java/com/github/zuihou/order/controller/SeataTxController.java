c0944cde16b2ed6092fa0064d59df5ac6a4457dd
tangyh <244387066@qq.com>
2020-01-02 16:58:10 +0800

    1，完善系统API&应用模块表结构、前后端对接等 2，操作日志详情 增加对模块描述 3，优化&修复前端项目bug、代码、命名等若干 4, 消息中心接口完善&前后端对接 5，删除附件统计相关无用接口 6，seata-all 相关版本升级到 1.0.0，完善demo&test服务对seata-server的支持(还存在问题） 7，升级 SpringCloud 版本为 Hoxton.SR1 8，升级 SpringBoot 版本为 2.2.2.RELEASE 9，升级 mybatis-plus 版本为 3.3.0 （升级后发现官方分页查询的bug，又回滚到3.2.0） 10，升级 Alibaba Cloud 版本为 2.1.1.RELEASE 11，升级 knife4j 版本为 2.0.1   （官方还存在一些bug） 12, 升级 hutool 版本为 5.1.0 13，升级Alibaba Cloud 版本为 2.1.1.RELEASE后， zuihou-j2cache-starter 存在无法读取nacos中配置的bug，已修复

--- zuihou-backend/zuihou-order/zuihou-order-controller/src/main/java/com/github/zuihou/order/controller/SeataTxController.java
+++ zuihou-backend/zuihou-order/zuihou-order-controller/src/main/java/com/github/zuihou/order/controller/SeataTxController.java
@@ -6,7 +6,6 @@ import com.github.zuihou.base.R;
 import com.github.zuihou.order.api.DemoTestApi;
 import com.github.zuihou.order.entity.Order;
 import com.github.zuihou.order.service.OrderService;
-
 import io.seata.spring.annotation.GlobalTransactional;
 import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
@@ -46,43 +45,55 @@ public class SeataTxController extends BaseController {
     @GlobalTransactional
     public R<Org> saveCommitSuccess(@RequestBody Org data) {
         log.info("data={}", data);
-        seataTestApi.save(data);
+        this.seataTestApi.save(data);
         Order entity = Order.builder()
                 .code(data.getAbbreviation())
                 .name(data.getName())
                 .build();
-        orderService.save(entity);
-        return success(data);
+        this.orderService.save(entity);
+        return this.success(data);
     }
 
     /**
      * 失败的回滚！  因为没有 @GlobalTransactional 注解
+     *
      * @param data
      * @return
      */
     @PostMapping("/save/rollback/fail")
     public R<Org> saveRollbackFail(@RequestBody Org data) {
         log.info("data={}", data);
-        seataTestApi.save(data);
+        this.seataTestApi.save(data);
         int i = 1 / 0;
         Order entity = Order.builder()
                 .code(data.getAbbreviation())
                 .name(data.getName())
                 .build();
-        orderService.save(entity);
-        return success(data);
+        this.orderService.save(entity);
+        return this.success(data);
     }
 
     /**
      * 正常回滚, 是分布式事务回滚的效果
-     * @param data
+     * <p>
+     * 目前存在2个问题：
+     * 1， seata-all 1.0.0 SeataHystrixConcurrencyStrategy 和 本项目中 ThreadLocalHystrixConcurrencyStrategy 冲突，导致事务回滚失效。
+     * 临时解决方案是将 ThreadLocalHystrixConcurrencyStrategy 中内部类 WrappedCallable 中注释的4行代码释放开
+     * <p>
+     * 2，insert into zuihou_base_0000.m_product 语法，还是无法回滚， 想要测试，请勿在请求头中传递 tenant 参数
+     *
+     * @param
      * @return
      */
     @PostMapping("/save/rollback")
     @GlobalTransactional
-    public R<Org> saveRollbackSuccess(@RequestBody Org data) {
+    public Boolean placeOrderRollback() {
+        Org data = Org.builder()
+                .name("你的名字")
+                .abbreviation("aa")
+                .build();
         log.info("data={}", data);
-        seataTestApi.save(data);
+        R<Org> save = this.seataTestApi.save(data);
 
         //在这里打断点可以看到 m_product 表的数据已经插入
         //但等执行完整个方法，发现 m_product 数据被删除
@@ -91,13 +102,14 @@ public class SeataTxController extends BaseController {
                 .code(data.getAbbreviation())
                 .name(data.getName())
                 .build();
-        orderService.save(entity);
+        this.orderService.save(entity);
 
-        return success(data);
+        return true;
     }
 
     /**
      * 正常回滚， 是demo服务的本地事务回滚的效果
+     *
      * @param data
      * @return
      */
@@ -105,13 +117,13 @@ public class SeataTxController extends BaseController {
     @GlobalTransactional
     public R<Org> saveRollbackSuccess2(@RequestBody Org data) {
         log.info("data={}", data);
-        seataTestApi.saveEx(data);
+        this.seataTestApi.saveEx(data);
 
         Order entity = Order.builder()
                 .code(data.getAbbreviation())
                 .name(data.getName())
                 .build();
-        orderService.save(entity);
-        return success(data);
+        this.orderService.save(entity);
+        return this.success(data);
     }
 }
--- zuihou-backend/zuihou-order/zuihou-order-server/src/main/java/com/github/zuihou/OrderServerApplication.java
+++ zuihou-backend/zuihou-order/zuihou-order-server/src/main/java/com/github/zuihou/OrderServerApplication.java
@@ -1,33 +1,31 @@
 package com.github.zuihou;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
 import com.github.zuihou.user.annotation.EnableLoginArgResolver;
 import com.github.zuihou.validator.config.EnableFormValidator;
-
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.Environment;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * 启动类
  *
  * @author zuihou
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableDiscoveryClient
 @EnableHystrix
 @EnableFeignClients(value = {
         "com.github.zuihou",
 })
-@EnableTransactionManagement
 @Slf4j
 @EnableLoginArgResolver
 @EnableFormValidator
