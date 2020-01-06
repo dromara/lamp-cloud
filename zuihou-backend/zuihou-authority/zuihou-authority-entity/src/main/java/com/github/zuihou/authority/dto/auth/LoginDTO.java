07cf2255fadbc2c22a08d752b81e606cd55a4323
tangyh <244387066@qq.com>
2020-01-06 09:50:51 +0800

    1，修复zuul加入 okhttp 拦截器记录日志后，请求报错的bug 2，修改登录接口为post正规接口

--- zuihou-backend/zuihou-authority/zuihou-authority-entity/src/main/java/com/github/zuihou/authority/dto/auth/LoginDTO.java
+++ zuihou-backend/zuihou-authority/zuihou-authority-entity/src/main/java/com/github/zuihou/authority/dto/auth/LoginDTO.java
@@ -1,18 +1,13 @@
 package com.github.zuihou.authority.dto.auth;
 
+import com.github.zuihou.auth.utils.Token;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.*;
+import lombok.experimental.Accessors;
+
 import java.io.Serializable;
 import java.util.List;
-
-import com.github.zuihou.auth.utils.Token;
-
-import io.swagger.annotations.ApiModel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-import lombok.experimental.Accessors;
 
 /**
  * 登录返回信息
@@ -29,7 +24,11 @@ import lombok.experimental.Accessors;
 @Builder
 @ApiModel(value = "LoginDTO", description = "登录信息")
 public class LoginDTO implements Serializable {
+    private static final long serialVersionUID = -3124612657759050173L;
+    @ApiModelProperty(value = "用户信息")
     private UserDTO user;
+    @ApiModelProperty(value = "token")
     private Token token;
+    @ApiModelProperty(value = "权限列表")
     private List<String> permissionsList;
 }
