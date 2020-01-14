d659ffff3cf40a3aad87d2092e7c85495039df1d
tangyh <244387066@qq.com>
2020-01-14 16:56:53 +0800

    1. 升级 zipkin-server 到 2.19.2 ，并修改项目配置，使得兼容新版本，并完善使用文档 3. 同步 zuihou-admin-boot 和 zuihou-admin-cloud 项目之间的代码，并部署到新的测试线 2. 优化 jobs项目的配置

--- zuihou-commons/zuihou-core/src/main/java/com/github/zuihou/base/entity/Entity.java
+++ zuihou-commons/zuihou-core/src/main/java/com/github/zuihou/base/entity/Entity.java
@@ -1,18 +1,17 @@
 package com.github.zuihou.base.entity;
 
-import java.time.LocalDateTime;
-
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
-
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import lombok.experimental.Accessors;
 
+import java.time.LocalDateTime;
+
 /**
- * 基础试题
+ * 基础实体
  *
  * @author zuihou
  * @date 2019/05/05
@@ -25,6 +24,7 @@ public class Entity<T> extends SuperEntity<T> {
 
     public static final String UPDATE_TIME = "updateTime";
     public static final String UPDATE_USER = "updateUser";
+    private static final long serialVersionUID = 5169873634279173683L;
 
     @ApiModelProperty(value = "最后修改时间")
     @TableField(value = "update_time", fill = FieldFill.INSERT_UPDATE)
