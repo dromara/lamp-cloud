9c15d044e5239529f6dda6a54a0039de5f1962ec
tangyh <244387066@qq.com>
2020-01-15 15:48:20 +0800

    MetaObjectHandler 支持任意名称的Id字段自动注入

--- zuihou-commons/zuihou-databases/src/main/java/com/github/zuihou/database/datasource/MyMetaObjectHandler.java
+++ zuihou-commons/zuihou-databases/src/main/java/com/github/zuihou/database/datasource/MyMetaObjectHandler.java
@@ -1,8 +1,14 @@
 package com.github.zuihou.database.datasource;
 
+import java.lang.reflect.Field;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
+import cn.hutool.core.util.ObjectUtil;
+import cn.hutool.core.util.ReflectUtil;
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import com.baomidou.mybatisplus.core.metadata.TableInfo;
+import com.baomidou.mybatisplus.core.metadata.TableInfoHelper;
 import com.baomidou.mybatisplus.core.toolkit.Constants;
 import com.github.zuihou.base.entity.Entity;
 import com.github.zuihou.base.entity.SuperEntity;
@@ -15,6 +21,7 @@ import org.apache.ibatis.reflection.MetaObject;
 /**
  * MyBatis Plus 元数据处理类
  * 用于自动 注入 id, createTime, updateTime, createUser, updateUser 等字段
+ *
  *
  * @author zuihou
  * @date 2019/04/29
@@ -37,16 +44,22 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
     }
 
     /**
+     * 注意：不支持 复合主键 自动注入！！
+     *
      * 所有的继承了Entity、SuperEntity的实体，在insert时，
-     * id： 会通过IdGenerate生成唯一ID
+     * id： id为空时， 通过IdGenerate生成唯一ID， 不为空则使用传递进来的id
      * createUser, updateUser: 自动赋予 当前线程上的登录人id
      * createTime, updateTime: 自动赋予 服务器的当前时间
+     *
+     * 未继承任何父类的实体，且主键标注了 @TableId(value = "xxx", type = IdType.INPUT) 自动注入 主键
+     * 主键的字段名称任意
      *
      * @param metaObject
      */
     @Override
     public void insertFill(MetaObject metaObject) {
         boolean flag = true;
+        // 设置创建时间和创建人
         if (metaObject.getOriginalObject() instanceof SuperEntity) {
             Object oldId = ((SuperEntity) metaObject.getOriginalObject()).getId();
             if (oldId != null) {
@@ -66,19 +79,47 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
             }
         }
 
-        if (flag) {
-            Long id = idGenerator.generate();
-            if (ID_TYPE.equals(metaObject.getGetterType(SuperEntity.FIELD_ID).getName())) {
-                this.setFieldValByName(SuperEntity.FIELD_ID, String.valueOf(id), metaObject);
-            } else {
-                this.setFieldValByName(SuperEntity.FIELD_ID, id, metaObject);
-            }
-        }
-
+        // 修改人 修改时间
         if (metaObject.getOriginalObject() instanceof Entity) {
             Entity entity = (Entity) metaObject.getOriginalObject();
             update(metaObject, entity);
         }
+
+        // 若 ID 中有值，就不设置
+        if (!flag) {
+            return;
+        }
+        Long id = idGenerator.generate();
+        if (metaObject.hasGetter(SuperEntity.FIELD_ID)) {
+            Object idVal = ID_TYPE.equals(metaObject.getGetterType(SuperEntity.FIELD_ID).getName()) ? String.valueOf(id) : id;
+            this.setFieldValByName(SuperEntity.FIELD_ID, idVal, metaObject);
+            return;
+        }
+
+        // 实体没有继承 Entity 和 SuperEntity
+        TableInfo tableInfo = metaObject.hasGetter(Constants.MP_OPTLOCK_ET_ORIGINAL) ?
+                TableInfoHelper.getTableInfo(metaObject.getValue(Constants.MP_OPTLOCK_ET_ORIGINAL).getClass())
+                : TableInfoHelper.getTableInfo(metaObject.getOriginalObject().getClass());
+        if (tableInfo == null) {
+            return;
+        }
+        // 主键类型
+        Class<?> keyType = tableInfo.getKeyType();
+        if (keyType == null) {
+            return;
+        }
+        // id 字段名
+        String keyProperty = tableInfo.getKeyProperty();
+
+        // 反射得到 主键的值
+        Field idField = ReflectUtil.getField(metaObject.getOriginalObject().getClass(), keyProperty);
+        Object fieldValue = ReflectUtil.getFieldValue(metaObject.getOriginalObject(), idField);
+        // 判断ID 是否有值，有值就不
+        if (ObjectUtil.isNotEmpty(fieldValue)) {
+            return;
+        }
+        Object idVal = keyType.getName().equalsIgnoreCase(ID_TYPE) ? String.valueOf(id) : id;
+        this.setFieldValByName(keyProperty, idVal, metaObject);
     }
 
     private void update(MetaObject metaObject, Entity entity, String et) {
