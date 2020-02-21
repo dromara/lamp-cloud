66bc49bc0f68b7e07db7231c4612d5ae2da7cc91
tangyh <244387066@qq.com>
2020-02-21 17:58:38 +0800

    完善 Super 和代码生成器

--- zuihou-commons/zuihou-boot/src/main/java/com/github/zuihou/base/service/SuperService.java
+++ zuihou-commons/zuihou-boot/src/main/java/com/github/zuihou/base/service/SuperService.java
@@ -5,15 +5,13 @@ import com.baomidou.mybatisplus.extension.toolkit.SqlHelper;
 import com.github.zuihou.base.mapper.SuperMapper;
 
 import java.io.Serializable;
-import java.util.Collection;
 import java.util.List;
 
 /**
- * 基于MP的 IService 新增了4个方法： saveBatchSomeColumn、updateAllById、getByIdCache、removeByIdsCache
+ * 基于MP的 IService 新增了3个方法： saveBatchSomeColumn、updateAllById、getByIdCache
  * 其中：
  * 1，updateAllById 执行后，会清除缓存
  * 2，getByIdCache 方法 会先从缓存查询，后从DB查询
- * 3，removeByIdsCache 方法支持清除缓存
  *
  * @param <T> 实体
  */
@@ -39,14 +37,6 @@ public interface SuperService<T> extends IService<T> {
      * @return
      */
     boolean updateAllById(T entity);
-
-    /**
-     * 根据id集合删除数据
-     *
-     * @param idList
-     * @return
-     */
-    boolean removeByIdsCache(Collection<? extends Serializable> idList);
 
     /**
      * 根据id 先查缓存，再查db
