080b6c1736f9f4adbfa8a8de5bf21aa260d051aa
tangyh <244387066@qq.com>
2020-04-26 00:24:57 +0800

    0. spring.cloud.version -> Hoxton.SR3 1. spring.boot.version -> 2.2.6.RELEASE 2. spring-cloud-alibaba -> 2.2.1.RELEASE 3. seata.version -> 1.1.0 4. fastjson.version -> 1.2.68 5. j2cache.version -> 2.8.1-release 6. jjwt.version -> 0.9.1 7. hutool.version -> 5.3.1 8. 支持 nacos 1.2.1 9. 支持 seata 1.2.0 10. 优化数据权限逻辑 11. 修复删除资源时执行删除SQL的bug 12. 修复查询缓存数据时，查询到为null的数据的会报NPE的bug 13. 同步 nacos 和 seata 的脚本 14. 修复RoleAuthorityServiceImpl类的方法 无事务注解 15. 优化项目配置，使得任意模式完美支持 seata 16. 优化若干代码

--- zuihou-authority/zuihou-authority-biz/src/main/java/com/github/zuihou/authority/service/common/DictionaryItemService.java
+++ zuihou-authority/zuihou-authority-biz/src/main/java/com/github/zuihou/authority/service/common/DictionaryItemService.java
@@ -20,10 +20,16 @@ public interface DictionaryItemService extends SuperCacheService<DictionaryItem>
     /**
      * 根据类型查询字典
      *
+     * @param types
+     * @return
+     */
+    Map<String, Map<String, String>> map(String[] types);
+
+    /**
+     * 根据类型编码查询字典项
+     *
      * @param codes
      * @return
      */
-    Map<String, Map<String, String>> map(String[] codes);
-
     Map<Serializable, Object> findDictionaryItem(Set<Serializable> codes);
 }
