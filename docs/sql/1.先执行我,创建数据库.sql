ee1ab2c6a8996cffce11999830f98e99b1e5f73e
tangyh <244387066@qq.com>
2020-10-19 17:53:54 +0800

    docs: 文档说明写错

--- docs/sql/1.先执行我,创建数据库.sql
+++ docs/sql/1.先执行我,创建数据库.sql
@@ -1,7 +1,8 @@
 CREATE DATABASE IF NOT EXISTS `zuihou_defaults` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 CREATE DATABASE IF NOT EXISTS `zuihou_base_0000` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 CREATE DATABASE IF NOT EXISTS `zuihou_extend_0000` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-CREATE DATABASE IF NOT EXISTS `zuihou_column` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE DATABASE IF NOT EXISTS `zuihou_column_all` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE DATABASE IF NOT EXISTS `zuihou_none` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
 CREATE DATABASE IF NOT EXISTS `zuihou_zipkin` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 CREATE DATABASE IF NOT EXISTS `zuihou_nacos_121` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
