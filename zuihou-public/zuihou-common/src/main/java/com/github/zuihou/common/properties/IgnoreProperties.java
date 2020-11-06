4dc76f5dd281a019b94ded4dc57b91452b161940
tangyh <244387066@qq.com>
2020-11-06 21:14:22 +0800

    build: 2.7.0
    - fix: 修复任务启动后，更新任务触发时间，不生效  fix:29
    - fix: redis持久化策略不影响全局配置
    - fix: 修复uid模块启动可能报错的bug
    - feat: 合并ContextHandlerInterceptor、TenantContextHandlerInterceptor、MdcHandlerInterceptor 为 HeaderThreadLocalInterceptor， 专门用于处理请求头中的参数封装到ThreadLocal
    - feat: TokenContextFilter支持单独配置忽略tenant、忽略token
    - feat: IgnoreTokenProperties 重命名为 IgnoreProperties，并将原来的配置`ignore.token.url`改成`zuihou.ignore.xxx`
    - feat: 废弃无用的单元测试
    - feat: apache的StringUtils替换为hutool的StrUtil
    - feat: xss-starter相关功能支持通过配置文件配置
    - feat: SysLog注解支持获取当前线程上下文中的参数.  用法 #threadLocal.name
    - feat: 数据注入代码结构、注释优化
    - feat: 数据注入支持忽略注入指定字段
        ```
        injectionCore.injection(user, "org", "nation");
        ```
    - feat: 代码注释完善若干、代码逻辑优化若干、修复bug若干

---
+++ zuihou-public/zuihou-common/src/main/java/com/github/zuihou/common/properties/IgnoreProperties.java
@@ -0,0 +1,62 @@
+package com.github.zuihou.common.properties;
+
+import cn.hutool.core.collection.CollUtil;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.util.AntPathMatcher;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 忽略token 配置类
+ * <p>
+ * 做接口权限时，考虑修改成读取配置文件
+ *
+ * @author zuihou
+ * @date 2019/01/03
+ */
+@Data
+@ConfigurationProperties(prefix = IgnoreProperties.PREFIX)
+public class IgnoreProperties {
+    public static final String PREFIX = "zuihou.ignore";
+    private static final AntPathMatcher ANT_PATH_MATCHER = new AntPathMatcher();
+
+    private List<String> baseUri = CollUtil.newArrayList(
+            "/**/*.css",
+            "/**/*.js",
+            "/**/*.html",
+            "/**/*.ico",
+            "/**/*.jpg",
+            "/**/*.jpeg",
+            "/**/*.png",
+            "/**/*.gif",
+            "/**/v2/**",
+            "/**/swagger-resources/**",
+            "/**/webjars/**",
+            "/actuator/**",
+            "/error",
+            "/**/anno/**",
+            "/**/static/**",
+            "/**/login"
+    );
+
+    private List<String> tenant = CollUtil.newArrayList();
+    private List<String> token = CollUtil.newArrayList();
+
+    public boolean isIgnoreToken(String path) {
+        List<String> all = new ArrayList<>();
+        all.addAll(getBaseUri());
+        all.addAll(getToken());
+        return all.stream().anyMatch(url -> path.startsWith(url) || ANT_PATH_MATCHER.match(url, path));
+    }
+
+    public boolean isIgnoreTenant(String path) {
+        List<String> all = new ArrayList<>();
+        all.addAll(getBaseUri());
+        all.addAll(getTenant());
+        return all.stream().anyMatch(url -> path.startsWith(url) || ANT_PATH_MATCHER.match(url, path));
+    }
+
+
+}
