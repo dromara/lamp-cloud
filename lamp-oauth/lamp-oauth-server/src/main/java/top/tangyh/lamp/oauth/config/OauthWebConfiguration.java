2ea05bd46de9a38fe4df150bff64023a6005e984
最后 <306479353@qq.com>
2021-08-18 11:01:21 +0800

    fix: SystemProperties

--- lamp-oauth/lamp-oauth-server/src/main/java/top/tangyh/lamp/oauth/config/OauthWebConfiguration.java
+++ lamp-oauth/lamp-oauth-server/src/main/java/top/tangyh/lamp/oauth/config/OauthWebConfiguration.java
@@ -1,17 +1,20 @@
 package top.tangyh.lamp.oauth.config;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import top.tangyh.basic.boot.config.BaseConfig;
 import top.tangyh.basic.log.event.SysLogListener;
 import top.tangyh.lamp.authority.service.common.OptLogService;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import top.tangyh.lamp.common.properties.SystemProperties;
 
 /**
  * @author zuihou
  * @date 2017-12-15 14:42
  */
 @Configuration
+@EnableConfigurationProperties({SystemProperties.class})
 public class OauthWebConfiguration extends BaseConfig {
 
     /**
