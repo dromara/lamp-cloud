57dcddd37460cad6fd592a6028d03e79b3fc630f
zuihou <244387066@qq.com>
2021-11-28 23:49:21 +0800

    refactor: 调整TraceFilter执行顺序,便于网关其他过滤器记录日志信息

--- lamp-gateway/lamp-gateway-server/src/main/java/top/tangyh/lamp/gateway/filter/TraceFilter.java
+++ lamp-gateway/lamp-gateway-server/src/main/java/top/tangyh/lamp/gateway/filter/TraceFilter.java
@@ -1,15 +1,15 @@
 package top.tangyh.lamp.gateway.filter;
 
 import cn.hutool.core.util.IdUtil;
-import top.tangyh.basic.context.ContextConstants;
 import org.slf4j.MDC;
-import org.springframework.cloud.gateway.filter.GatewayFilterChain;
-import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
+import top.tangyh.basic.context.ContextConstants;
 
 /**
  * 生成日志链路追踪id，并传入header中
@@ -18,10 +18,10 @@ import reactor.core.publisher.Mono;
  * @date 2020年03月09日18:02:47
  */
 @Component
-public class TraceFilter implements GlobalFilter, Ordered {
+public class TraceFilter implements WebFilter, Ordered {
 
     @Override
-    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
         //链路追踪id
         String traceId = IdUtil.fastSimpleUUID();
         MDC.put(ContextConstants.LOG_TRACE_ID, traceId);
