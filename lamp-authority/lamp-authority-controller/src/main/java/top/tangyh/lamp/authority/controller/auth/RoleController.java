4720d236ef52ae29c2719a30d3b09d79a1324ae5
zuihou <244387066@qq.com>
2022-01-28 16:29:29 +0800

    build: 3.6.0
    新增数据权限功能

--- lamp-authority/lamp-authority-controller/src/main/java/top/tangyh/lamp/authority/controller/auth/RoleController.java
+++ lamp-authority/lamp-authority-controller/src/main/java/top/tangyh/lamp/authority/controller/auth/RoleController.java
@@ -82,6 +82,8 @@ public class RoleController extends SuperCacheController<RoleService, Long, Role
                 .in(Role::getReadonly, roleQuery.getReadonly())
                 .in(Role::getCategory, roleQuery.getCategory());
         baseService.page(page, wrapper);
+
+        handlerResult(page);
         return page;
     }
 
