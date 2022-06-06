8666275317584db722c9655f09011ed4904155aa
zuihou <244387066@qq.com>
2022-06-06 09:36:50 +0800

    feat: 注释注解

--- lamp-public/lamp-userinfo-sdk/src/main/java/top/tangyh/lamp/userinfo/dao/UserHelperMapper.java
+++ lamp-public/lamp-userinfo-sdk/src/main/java/top/tangyh/lamp/userinfo/dao/UserHelperMapper.java
@@ -1,6 +1,5 @@
 package top.tangyh.lamp.userinfo.dao;
 
-import com.baomidou.mybatisplus.annotation.InterceptorIgnore;
 import org.springframework.stereotype.Repository;
 import top.tangyh.basic.base.mapper.SuperMapper;
 import top.tangyh.lamp.model.entity.base.SysUser;
@@ -12,7 +11,6 @@ import top.tangyh.lamp.model.entity.base.SysUser;
  * @create [2022/4/24 11:25 AM ] [tangyh] [初始创建]
  */
 @Repository
-@InterceptorIgnore(tenantLine = "true", dynamicTableName = "true")
 public interface UserHelperMapper extends SuperMapper<SysUser> {
 
 }
