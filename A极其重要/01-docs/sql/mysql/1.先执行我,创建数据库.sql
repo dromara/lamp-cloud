d8ef2f9aab4390c09d3d941ac5a64d9282924926
zuihou <244387066@qq.com>
2024-01-12 11:05:47 +0800

    build: 4.16.0-java17

--- A极其重要/01-docs/sql/mysql/1.先执行我,创建数据库.sql
+++ A极其重要/01-docs/sql/mysql/1.先执行我,创建数据库.sql
@@ -1,12 +1,12 @@
 /*
- 脚本是mysql 8 导出的，如果你用的是mysql7 需要填调整脚本： 将 utf8mb4_0900_ai_ci 全部替换成 utf8mb4_general_ci
+ 脚本是mysql 8 导出的，如果你用的是mysql5.7 需要填调整脚本： 将 utf8mb4_0900_ai_ci 全部替换成 utf8mb4_general_ci
  */
 -- nacos 数据库
 CREATE DATABASE IF NOT EXISTS `lamp_nacos` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 -- SkyWalking 数据库
 CREATE DATABASE IF NOT EXISTS `lamp_sw` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
--- lamp-cloud-pro-none 和 lamp-boot-pro-none 数据库
+-- lamp-cloud-none 和 lamp-boot-none 数据库
 CREATE DATABASE IF NOT EXISTS `lamp_none` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
 
