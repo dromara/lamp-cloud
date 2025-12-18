fc937eab1691042deb60a00e788db599b0f45584
zuihou <244387066@qq.com>
2025-12-19 00:05:35 +0800

    fix(generator): 修正依赖版本占位符转义问题
    - 将依赖版本标签中的转义符号\\$去除，改为正常的${revision}

--- lamp-generator/lamp-generator-biz/src/main/java/top/tangyh/lamp/generator/utils/ProjectUtils.java
+++ lamp-generator/lamp-generator-biz/src/main/java/top/tangyh/lamp/generator/utils/ProjectUtils.java
@@ -246,7 +246,7 @@ public class ProjectUtils {
                                     <dependency>
                                         <groupId>{}</groupId>
                                         <artifactId>{}-{}-controller</artifactId>
-                                        <version>\\${revision}</version>
+                                        <version>${revision}</version>
                                     </dependency>
                             """,
                     vo.getGroupId(), projectPrefix, vo.getServiceName());
